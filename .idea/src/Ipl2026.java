2f2f1e7ac4f5cb48f1e7c1de1040649583acbfd8
sanja <sanjay.konda24@gmail.com>
2024-06-30 10:42:08 +0530

    ipl2026-2 last two lines changes1

--- .idea/src/Ipl2026.java
+++ .idea/src/Ipl2026.java
@@ -6,6 +6,23 @@ public class Ipl2026 {
 
     public String name2="pawan";
 
+    public String name3="vera";
+
+    public String name4="dhana";
+
+
+    public String name5="vera2";
+
+    public String name6="dhana3";
+
+
+
+
+
+    public String name7="veraiah";
+
+
+    public String name8="laxmipathi";
 
 
 }
